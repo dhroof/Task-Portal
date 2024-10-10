bd4ffa98d1ff01e128dfb4505fd2b0bfd4d1934b
Dhruv Mehta <dhruvmehta09.01@gmail.com>
2024-10-10 19:58:56 +0530

    Added Comments

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const dotenv = require("dotenv").config();
 const connectDb = require("./config/db");
 const port = process.env.PORT;
 
+// Connect to the database
 connectDb();
+
+// Parse JSON request bodies
 app.use(express.json());
-app.use("/api/user", require("./routes/userRoute"));
-app.use("/api/admin", require("./routes/adminRoute"));
+
+app.use("/api/user", require("./routes/userRoute")); // User routes
+app.use("/api/admin", require("./routes/adminRoute")); // Admin routes
 
 app.listen(port, () => {
   console.log("Server running on port:", port);
