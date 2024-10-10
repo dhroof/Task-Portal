bb294768b9ce49876627cd0d6165cf74fc620375
Dhruv Mehta <dhruvmehta09.01@gmail.com>
2024-10-10 19:37:49 +0530

    Create assignmentModel.js

---
+++ models/assignmentModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const assignmentSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    task: {
+      type: String,
+      required: true,
+    },
+    adminId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Assignment = mongoose.model("Assignment", assignmentSchema);
+
+module.exports = Assignment;
